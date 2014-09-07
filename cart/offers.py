44deea371886de1581081e5de85c76d203fe651a
Dave Halter <davidhalter88@gmail.com>
2014-09-08 01:05:48 +0200

    ThreeForTwo and TwoForOne doctests.

--- cart/offers.py
+++ cart/offers.py
@@ -2,10 +2,13 @@
 ``cart.Offer`` implementations.
 """
 
-from cart import Offer
+import cart
 
 
-class XForY(Offer):
+class XForY(cart.Offer):
+    """
+    Buy X, pay for Y.
+    """
     def __init__(self, product_name, x, y):
         self._product_name = product_name
         self._x = x
@@ -18,11 +21,37 @@ class XForY(Offer):
         cart._product_prices
 
 
-class ThreeForTwo(Offer):
+class ThreeForTwo(cart.Offer):
+    """
+    Buy three, pay for two.
+
+    >>> c = cart.Cart({'strawberries': 1})
+    >>> c.add_to_cart('strawberries', 1)
+    >>> assert c.price == 1
+    >>> c.add_to_cart('strawberries', 1)
+    >>> assert c.price == 2
+    >>> c.add_to_cart('strawberries', 1)
+    >>> assert c.price == 2
+    >>> c.add_to_cart('strawberries', 1)
+    >>> assert c.price == 3
+    """
     def __init__(self, product_name):
         super(ThreeForTwo, self).__init__(product_name, 2, 3)
 
 
-class TwoForOne(Offer):
+class TwoForOne(cart.Offer):
+    """
+    Buy two, pay for one.
+
+    >>> c = cart.Cart({'icecream': 1})
+    >>> c.add_to_cart('icecream', 1)
+    >>> assert c.price == 1
+    >>> c.add_to_cart('icecream', 1)
+    >>> assert c.price == 1
+    >>> c.add_to_cart('icecream', 1)
+    >>> assert c.price == 2
+    >>> c.add_to_cart('icecream', 1)
+    >>> assert c.price == 2
+    """
     def __init__(self, product_name):
         super(TwoForOne, self).__init__(product_name, 1, 2)
