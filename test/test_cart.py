82365df14ba23802c142b6bb1e049b19ddde4e5c
Dave Halter <davidhalter88@gmail.com>
2014-09-08 00:24:29 +0200

    Tests for Cart.add_to_cart.

--- test/test_cart.py
+++ test/test_cart.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import csv
-
 import cart
 from cart._compatibility import utf8
 
 import pytest
 
+
+# ---------
+#  cart_from_csv tests
+# ---------
 
 def test_valid_cart_from_csv():
     _cart = cart.cart_from_csv('test/cart_files/default_cart.csv')
@@ -29,3 +31,17 @@ def test_non_existing_cart_from_csv_file():
 def test_invalid_cart_from_csv_file():
     with pytest.raises(cart.MalformedCSV):
         cart.cart_from_csv('test/cart_files/invalid_cart.csv')
+
+# ---------
+#  Cart tests
+# ---------
+
+def test_add_to_cart():
+    def content(prices, name, quantity):
+        c = cart.Cart(prices)
+        c.add_to_cart(name, quantity)
+        return c._added_products
+
+    assert content({'apple': 1.0}, 'apple', 2) == {'apple', 2}
+    with pytest.raises(KeyError):
+        assert content({}, 'apple', 2) == {'apple', 2}
