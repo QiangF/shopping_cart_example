3dbdc8581bcb366e5b6749d523f25589d3ede9ed
Dave Halter <davidhalter88@gmail.com>
2014-09-08 00:06:00 +0200

    Add tests for unicode csv files.

--- test/test_cart.py
+++ test/test_cart.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import cart
+from cart._compatibility import utf8
+
 
 def test_valid_cart_from_csv():
     _cart = cart.cart_from_csv('test/cart_files/default_cart.csv')
@@ -6,3 +10,8 @@ def test_valid_cart_from_csv():
                                      'ice cream': 3.49,
                                      'strawberries': 2.00,
                                      'snickers bar': 0.70}
+
+
+def test_unicode_cart_from_csv():
+    _cart = cart.cart_from_csv('test/cart_files/unicode_cart.csv')
+    assert _cart._product_prices == {utf8('somethingä'): 1.0}
